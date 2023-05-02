1bc28240fd7cbba5787ad10171a0cee62eb7ab3e
ElenaGavrina <45360118+ElenaGavrina@users.noreply.github.com>
2023-05-02 22:57:48 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,49 +1,59 @@
 package main
 
 import (
-	"encoding/base64"
-	"fmt"
 	"flag"
+	"log"
+	"io/ioutil"
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/rand"
-	"io/ioutil"
 	"io"
-	"log"
+	"fmt"
+	"encoding/base64"
 )
 
 var mes = flag.String("mes","text","text message")
 var mode = flag.Bool("mode",true,"encode")
 var key = flag.String("k","1234567890abcdef","key chipher")
-var file = flag.String("fp","myfile.txt","file path")
-
+var file = flag.String("fp","","file path")
 
 func main(){
 	flag.Parse()
-	if *file != ""{
-		encryptR,err := Encode(*key,fromFileToByte(*file));if err!= nil{
-			fmt.Println(err)
-		}	
-		fmt.Println(encryptR,nil)
-	}
-
-	if *mes !=""{
-		if *mode{
-			encryptResult, err := Encode(*key,fromMesToByte(*mes));if err!= nil{
-				fmt.Println(err)
-			}	
-			fmt.Println(encryptResult)
-		}else{
-	    	decryptResult, err := DecryptMessage(*key,*mes);if err!= nil{
-				fmt.Println(err)
+	if *mode {
+		if *file != ""{
+			encF := fromFileToByte(*file, *key)
+			err := ioutil.WriteFile("ciphertext.bin", []byte(encF), 0777)
+			if err != nil {
+				log.Fatalf("write file err: %v", err.Error())
 			}
-			fmt.Println(decryptResult)
+		}
+		if *mes != ""{
+			encM:=fromMesToByte(*mes, *key)
+			fmt.Println(encM)
 		}
 	}
-	
 }
 
-func Encode(key string, message []byte) (string,error) {
+func fromMesToByte(message string, key string)string{
+	byteMsg := []byte(message)
+	res,err:=encode(byteMsg, key)
+	if err!= nil{
+		return err.Error()
+	}
+	return res
+}
+func fromFileToByte(message string, key string)string{
+	plainText, err := ioutil.ReadFile(message)
+	if err != nil {
+		log.Fatalf("read file err: %v", err.Error())
+	}
+	res,err := encode(plainText, key)
+	if err!= nil{
+		return err.Error()
+	}
+	return res
+}
+func encode(message []byte,key string)(string,error){
 	block, err := aes.NewCipher([]byte(key))
 	if err != nil {
 		return "", fmt.Errorf("could not create new cipher: %v", err)
@@ -53,65 +63,7 @@ func Encode(key string, message []byte) (string,error) {
 	if _, err = io.ReadFull(rand.Reader, iv); err != nil {
 		return "", fmt.Errorf("could not encrypt: %v", err)
 	}
-
 	stream := cipher.NewCFBEncrypter(block, iv)
 	stream.XORKeyStream(cipherText[aes.BlockSize:], message)
 	return base64.StdEncoding.EncodeToString(message),nil
 }
-
-func DecryptMessage(key string,message string) (string, error) {
-	cipherText, err := base64.StdEncoding.DecodeString(message)
-	if err != nil {
-		return "", fmt.Errorf("could not base64 decode: %v", err)
-	}
-
-	block, err := aes.NewCipher([]byte(key))
-	if err != nil {
-		return "", fmt.Errorf("could not create new cipher: %v", err)
-	}
-	
-	iv := cipherText[:aes.BlockSize]
-	cipherText = cipherText[aes.BlockSize:]
-
-	stream := cipher.NewCFBDecrypter(block, iv)
-	stream.XORKeyStream(cipherText, cipherText)
-	return string(cipherText), nil
-}
-
-func decryptFile(key string) {
-	cipherText, err := ioutil.ReadFile("myfile.bin")
-	if err != nil {
-		log.Fatal(err)
-	}
-	block, err := aes.NewCipher([]byte(key))
-	if err != nil {
-		log.Fatalf("cipher err: %v", err.Error())
-	}
-	gcm, err := cipher.NewGCM(block)
-	if err != nil {
-		log.Fatalf("cipher GCM err: %v", err.Error())
-	}
-	nonce := cipherText[:gcm.NonceSize()]
-	cipherText = cipherText[gcm.NonceSize():]
-	plainText, err := gcm.Open(nil, nonce, cipherText, nil)
-	if err != nil {
-		log.Fatalf("decrypt file err: %v", err.Error())
-	}
-
-	err = ioutil.WriteFile("myfile.txt", plainText, 0777)
-	if err != nil {
-		log.Fatalf("write file err: %v", err.Error())
-	}
-}
-func fromMesToByte(message string)[]byte{
-	byteMsg := []byte(message)
-	return byteMsg
-}
-func fromFileToByte(message string)[]byte{
-	plainText, err := ioutil.ReadFile(message)
-	if err != nil {
-		log.Fatalf("read file err: %v", err.Error())
-	}
-	return plainText
-}
-
